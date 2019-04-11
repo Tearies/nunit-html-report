68cb091ac653efedb0cfd25500487d99c286aa21
Tearies <maojun_huier@sina.com>
2019-04-11 14:21:41 +0800

    翻译部分英语标签

--- templates/default/js/controllers/DashboardController.js
+++ templates/default/js/controllers/DashboardController.js
@@ -1,52 +1,66 @@
 var RESULT_TYPE_ANY = "Any";
 
 function DashboardController($scope, $location, $routeParams, StateStorage, reportAdapter) {
-	
-	$scope.testCaseResultFilter = function(testCase) {
+
+    $scope.testCaseResultFilter = function (testCase) {
         return $scope.isResultTypeActive(testCase.result) || $scope.isResultTypeAny();
     }
-    $scope.testCaseTextFilter = function(testCase) {
+    $scope.testCaseTextFilter = function (testCase) {
         matches = testCase.name.indexOf($scope.query) !== -1 || testCase.getMessage().indexOf($scope.query) !== -1
-        return $scope.excludeFilter ? !matches : matches;    
+        return $scope.excludeFilter ? !matches : matches;
     }
 
-    $scope.isResultTypeActive = function(resultType) {
+    $scope.isResultTypeActive = function (resultType) {
         return $scope.getActiveResultType() === resultType;
     };
 
-    $scope.isResultTypeAny = function(){
+    $scope.isResultTypeAny = function () {
         return $scope.isResultTypeActive(RESULT_TYPE_ANY);
     };
 
-    $scope.GetDisplayName=function(testCase){
+    $scope.GetDisplayName = function (testCase) {
         console.log(testCase.DisPlayName);
         return testCase.DisPlayName;
     };
-
-    $scope.setAnyResultType = function(){
+    /*
+        获取测试案例的测试结果
+    */
+    $scope.GetTestCaseResult = function (testCase) {
+        if (testCase.result == "Failed") {
+            return "失败";
+        }
+        if (testCase.result == "Passed") {
+            return "通过";
+        }
+        if (testCase.result == "Inconclusive") {
+            return "无效";
+        }
+        return "跳过";
+    };
+    $scope.setAnyResultType = function () {
         $scope.setActiveResultType(RESULT_TYPE_ANY);
     };
 
-    $scope.setActiveResultType = function(resultType) {
+    $scope.setActiveResultType = function (resultType) {
         StateStorage.put('ActiveResultType', resultType || RESULT_TYPE_ANY);
     };
 
-    $scope.getActiveResultType = function(){
+    $scope.getActiveResultType = function () {
         return StateStorage.get('ActiveResultType') || RESULT_TYPE_ANY;
     };
 
-    $scope.openTestCase = function(testCase) {
+    $scope.openTestCase = function (testCase) {
         $location.path('testcase/' + testCase.id);
     };
-    $scope.saveQueryState = function(){
+    $scope.saveQueryState = function () {
         StateStorage.put('query', $scope.query);
     }
 
-    $scope.saveExcludeFilterState = function(){
+    $scope.saveExcludeFilterState = function () {
         StateStorage.put('exclude', $scope.excludeFilter);
     }
 
-    $scope.setQueryFilterValue = function(item){
+    $scope.setQueryFilterValue = function (item) {
         $scope.query = item;
     };
 
@@ -63,9 +77,9 @@ function DashboardController($scope, $location, $routeParams, StateStorage, repo
         }
     };
 
-    $scope.currentTestCase = $routeParams.id 
-        ? reportAdapter.findTestCaseById($routeParams.id)
-        : $scope.testCases[0]; 
+    $scope.currentTestCase = $routeParams.id ?
+        reportAdapter.findTestCaseById($routeParams.id) :
+        $scope.testCases[0];
 
     $scope.query = StateStorage.get('query') || '';
     $scope.excludeFilter = StateStorage.get('exclude') || false;
